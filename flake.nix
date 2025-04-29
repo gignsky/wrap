9ed5f02ec3d3d427b241f09e62d9bd8faea945bd
gignsky <gig@gignsky.com>
2025-04-29 17:55:20 -0400

    updated formatting

--- flake.nix
+++ flake.nix
@@ -5,16 +5,24 @@
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    systems.url = "github:nix-systems/default";
+    systems = {
+      url = "github:nix-systems/default";
+    };
     rust-flake = {
       url = "github:juspay/rust-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
-    cargo-doc-live.url = "github:srid/cargo-doc-live";
+    process-compose-flake = {
+      url = "github:Platonic-Systems/process-compose-flake";
+    };
+    cargo-doc-live = {
+      url = "github:srid/cargo-doc-live";
+    };
 
     # Dev tools
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+    };
   };
 
   outputs = inputs:
