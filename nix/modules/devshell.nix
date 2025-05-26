549ded57b70f38cf9f91ff32665b418d5f9b9a34
gignsky <gig@gignsky.com>
2025-05-26 11:17:39 -0400

    added lolcat back to devShell

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -27,7 +27,7 @@
         inputs.dotfiles.packages.${system}.cargo-update
       ];
       shellHook = ''
-        echo "welcome to the rust development environment for the wrap package" | ${pkgs.cowsay}/bin/cowsay
+        echo "welcome to the rust development environment for the wrap package" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat 2> /dev/null;
       '';
     };
   };
