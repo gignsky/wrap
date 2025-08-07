a40b7e792b79795a034313c51b20a138cd8828f9
Maxwell Rupp <gig@gignsky.com>
2025-08-06 22:26:54 -0400

    Revert "added wrap to devShell"
    This reverts commit 4170c990b87615047a99cd9f559b34256a53017c.

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,42 +1,32 @@
 { inputs, ... }:
 {
-  perSystem =
-    { config
-    , self'
-    , pkgs
-    , lib
-    , ...
-    }:
-    {
-      devShells.default = pkgs.mkShell {
-        name = "wrap-shell";
-        inputsFrom = [
-          self'.devShells.rust
-          config.treefmt.build.devShell
-          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-        ];
-        packages = with pkgs; [
-          just
-          nixd # Nix language server
-          bacon
-          config.process-compose.cargo-doc-live.outputs.package
-          nil
-          lolcat
-          wslu
-          cargo-generate
-          python3
+  perSystem = { config, self', pkgs, lib, ... }: {
+    devShells.default = pkgs.mkShell {
+      name = "wrap-shell";
+      inputsFrom = [
+        self'.devShells.rust
+        config.treefmt.build.devShell
+        config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
+      ];
+      packages = with pkgs; [
+        just
+        nixd # Nix language server
+        bacon
+        config.process-compose.cargo-doc-live.outputs.package
+        nil
+        lolcat
+        wslu
+        cargo-generate
+        python3
 
-          # dotfiles programs
-          inputs.dotfiles.packages.${system}.quick-results
-          inputs.dotfiles.packages.${system}.upjust
-          inputs.dotfiles.packages.${system}.cargo-update
-
-          # wrapper programs
-          wrap
-        ];
-        shellHook = ''
-          echo "welcome to the rust development environment for the wrap package" | ${pkgs.cowsay}/bin/cowsay
-        '';
-      };
+        # dotfiles programs
+        inputs.dotfiles.packages.${system}.quick-results
+        inputs.dotfiles.packages.${system}.upjust
+        inputs.dotfiles.packages.${system}.cargo-update
+      ];
+      shellHook = ''
+        echo "welcome to the rust development environment for the wrap package" | ${pkgs.cowsay}/bin/cowsay
+      '';
     };
+  };
 }
