ca4a7b8c37600ba422116540b49bfaeb46d55cc7
Maxwell Rupp <gig@gignsky.com>
2025-08-06 23:49:48 -0400

    updated outputs

--- nix/modules/rust.nix
+++ nix/modules/rust.nix
@@ -1,35 +1,23 @@
-{ inputs, ... }:
-{
+{ inputs, ... }: {
   imports = [
     inputs.rust-flake.flakeModules.default
     inputs.rust-flake.flakeModules.nixpkgs
     inputs.process-compose-flake.flakeModule
     inputs.cargo-doc-live.flakeModule
   ];
-  perSystem =
-    { config
-    , self'
-    , pkgs
-    , lib
-    , ...
-    }:
-    {
-      rust-project.crates."wrap".crane.args = {
-        buildInputs = lib.optionals pkgs.stdenv.isDarwin (
-          with pkgs.darwin.apple_sdk.frameworks;
-          [
-            IOKit
-          ]
-        );
-      };
-      packages = {
-        default = self'.packages.wrap;
-        wrapper = pkgs.writeShellScriptBin "wrapper" ''
-          exec ${self'.packages.wrap}/bin/wrap "$@"
-        '';
-        wrapd = pkgs.writeShellScriptBin "wrapd" ''
-          exec ${self'.packages.wrap}/bin/wrap "$@"
-        '';
-      };
+  perSystem = { config, self', pkgs, lib, ... }: {
+    rust-project.crates."wrapd".crane.args = {
+      buildInputs = lib.optionals pkgs.stdenv.isDarwin
+        (with pkgs.darwin.apple_sdk.frameworks; [ IOKit ]);
     };
+    packages = {
+      default = self'.packages.wrapd;
+      wrapper = pkgs.writeShellScriptBin "wrapper" ''
+        exec ${self'.packages.wrapd}/bin/wrapd "$@"
+      '';
+      wrap = pkgs.writeShellScriptBin "wrap" ''
+        exec ${self'.packages.wrapd}/bin/wrapd "$@"
+      '';
+    };
+  };
 }
